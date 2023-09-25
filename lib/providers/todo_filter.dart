127ac33ec6b700823e0dcebddc467f721d9d0e52
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-09-25 22:30:16 +0200

    Create TodoFilterState with Filter variable, extends with Equatable, make it Stringify and create CopyWith function.
    Create initial state of TodoFilterState and give filter value to Filter.all

---
+++ lib/providers/todo_filter.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+import 'package:todo_provider/model/todo_model.dart';
+
+class TodoFilterState extends Equatable {
+  final Filter filter;
+  TodoFilterState({
+    required this.filter,
+  });
+
+  factory TodoFilterState.initial() {
+    return TodoFilterState(filter: Filter.all);
+  }
+
+  @override
+  List<Object> get props => [filter];
+
+  @override
+  bool get stringify => true;
+
+  TodoFilterState copyWith({
+    Filter? filter,
+  }) {
+    return TodoFilterState(
+      filter: filter ?? this.filter,
+    );
+  }
+}
