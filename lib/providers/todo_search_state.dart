b6458814831dfe283229a6d84aabb2a43b4804b7
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-09-26 21:40:13 +0200

    Creaate TodoSearch class with state getter and setSearchTerm function

--- lib/providers/todo_search_state.dart
+++ lib/providers/todo_search_state.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 class TodoSearchState extends Equatable {
   final String searchTerm;
@@ -23,5 +24,14 @@ class TodoSearchState extends Equatable {
     return TodoSearchState(
       searchTerm: searchTerm ?? this.searchTerm,
     );
+  }
+}
+
+class TodoSearch with ChangeNotifier {
+  TodoSearchState _state = TodoSearchState.initial();
+  TodoSearchState get state => _state;
+
+  void setSearchTerm(String newSearchTerm) {
+    _state = _state.copyWith(searchTerm: newSearchTerm);
   }
 }
