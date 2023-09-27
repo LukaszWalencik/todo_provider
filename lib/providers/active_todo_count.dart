51c7ade96ae4fc78edbf77fda0c03334874cdf10
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-09-27 21:39:25 +0200

    Create ActiveTodoCount mixin with CghangeNotifier, this class read state of ActivetodoCountstate and have function to update counter of active todos

--- lib/providers/active_todo_count.dart
+++ lib/providers/active_todo_count.dart
@@ -1,5 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_provider/model/todo_model.dart';
+import 'package:todo_provider/providers/todo_list.dart';
 
 class ActiveTodoCountState extends Equatable {
   final int activeTodoCount;
@@ -23,5 +26,20 @@ class ActiveTodoCountState extends Equatable {
     return ActiveTodoCountState(
       activeTodoCount: activeTodoCount ?? this.activeTodoCount,
     );
+  }
+}
+
+class ActiveTodoCount with ChangeNotifier {
+  ActiveTodoCountState _state = ActiveTodoCountState.intiial();
+  ActiveTodoCountState get state => _state;
+
+  void update(TodoList todoList) {
+    final int newActiveTodoCount = todoList.state.todos
+        .where((Todo todo) => !todo.isComplete)
+        .toList()
+        .length;
+
+    _state = _state.copyWith(activeTodoCount: newActiveTodoCount);
+    notifyListeners();
   }
 }
