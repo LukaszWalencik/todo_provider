4f59c7167ed4ef8e5b5c8ed900734d9ee81b99a4
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-09-27 21:59:01 +0200

    Create FilteredTodos class reading state from FilteredTodosState also create update function to update counter of todos depense of isCompleted value and search term

--- lib/providers/filtered_todos.dart
+++ lib/providers/filtered_todos.dart
@@ -1,7 +1,11 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 import 'package:todo_provider/model/todo_model.dart';
+import 'package:todo_provider/providers/todo_filter.dart';
+import 'package:todo_provider/providers/todo_list.dart';
+import 'package:todo_provider/providers/todo_search_state.dart';
 
 class FilteredTodosState extends Equatable {
   final List<Todo> filteredTodos;
@@ -25,5 +29,41 @@ class FilteredTodosState extends Equatable {
     return FilteredTodosState(
       filteredTodos: filteredTodos ?? this.filteredTodos,
     );
+  }
+}
+
+class FilteredTodos with ChangeNotifier {
+  FilteredTodosState _state = FilteredTodosState.initial();
+  FilteredTodosState get state => _state;
+
+  void update(
+    TodoFilter todoFilter,
+    TodoSearch todoSearch,
+    TodoList todoList,
+  ) {
+    List<Todo> _filteredTodos;
+    switch (todoFilter.state.filter) {
+      case Filter.active:
+        _filteredTodos = todoList.state.todos
+            .where((Todo todo) => !todo.isComplete)
+            .toList();
+        break;
+      case Filter.completed:
+        _filteredTodos =
+            todoList.state.todos.where((Todo todo) => todo.isComplete).toList();
+        break;
+      case Filter.all:
+      default:
+        _filteredTodos = todoList.state.todos;
+        break;
+    }
+    if (todoSearch.state.searchTerm.isNotEmpty) {
+      _filteredTodos = todoList.state.todos
+          .where((Todo todo) =>
+              todo.desc.toLowerCase().contains(todoSearch.state.searchTerm))
+          .toList();
+    }
+    _state = _state.copyWith(filteredTodos: _filteredTodos);
+    notifyListeners();
   }
 }
