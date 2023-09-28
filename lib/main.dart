d16cceb482f76af11141118c497e1c22a5c9f51a
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-09-28 14:53:18 +0200

    Add ChangeNotifierProviders in MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo_provider/pages/todos_page.dart';
+import 'package:todo_provider/providers/active_todo_count.dart';
+import 'package:todo_provider/providers/filtered_todos.dart';
+import 'package:todo_provider/providers/todo_filter.dart';
+import 'package:todo_provider/providers/todo_list.dart';
+import 'package:todo_provider/providers/todo_search_state.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +17,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Todos',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: TodosPage());
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<TodoFilter>(create: (context) => TodoFilter()),
+        ChangeNotifierProvider<TodoSearch>(create: (context) => TodoSearch()),
+        ChangeNotifierProvider<TodoList>(create: (context) => TodoList()),
+      ],
+      child: MaterialApp(
+          title: 'Todos',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: TodosPage()),
+    );
   }
 }
