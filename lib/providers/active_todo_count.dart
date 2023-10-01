0b021dd8533ec0010f45dc6c012dffac753f88fc
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-10-01 22:54:27 +0200

    Start changing from ChangeNotifierProxyProvider to ProxyProvider
    At start edit ActiveTodoCount

--- lib/providers/active_todo_count.dart
+++ lib/providers/active_todo_count.dart
@@ -1,8 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
-
 import 'package:todo_provider/model/todo_model.dart';
+
 import 'package:todo_provider/providers/todo_list.dart';
 
 class ActiveTodoCountState extends Equatable {
@@ -30,24 +29,14 @@ class ActiveTodoCountState extends Equatable {
   }
 }
 
-class ActiveTodoCount with ChangeNotifier {
-  // ActiveTodoCountState _state = ActiveTodoCountState.intiial();
-  final int initialActiveTodoCount;
-  late ActiveTodoCountState _state;
+class ActiveTodoCount {
+  final TodoList todoList;
   ActiveTodoCount({
-    required this.initialActiveTodoCount,
-  }) {
-    _state = ActiveTodoCountState(activeTodoCount: initialActiveTodoCount);
-  }
-  ActiveTodoCountState get state => _state;
-
-  void update(TodoList todoList) {
-    final int newActiveTodoCount = todoList.state.todos
-        .where((Todo todo) => !todo.isComplete)
-        .toList()
-        .length;
-
-    _state = _state.copyWith(activeTodoCount: newActiveTodoCount);
-    notifyListeners();
-  }
+    required this.todoList,
+  });
+  ActiveTodoCountState get state => ActiveTodoCountState(
+      activeTodoCount: todoList.state.todos
+          .where((Todo todo) => !todo.isComplete)
+          .toList()
+          .length);
 }
