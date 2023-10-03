914871052b82ceb5dab029f6e76ffe3237547f93
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-10-03 22:01:34 +0200

    Edit ActiveTodoCount from ChangeNotifier to StateNotifier

--- lib/providers/active_todo_count.dart
+++ lib/providers/active_todo_count.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
+import 'package:state_notifier/state_notifier.dart';
 import 'package:todo_provider/model/todo_model.dart';
 
 import 'package:todo_provider/providers/todo_list.dart';
@@ -29,14 +30,21 @@ class ActiveTodoCountState extends Equatable {
   }
 }
 
-class ActiveTodoCount {
-  final TodoList todoList;
-  ActiveTodoCount({
-    required this.todoList,
-  });
-  ActiveTodoCountState get state => ActiveTodoCountState(
-      activeTodoCount: todoList.state.todos
-          .where((Todo todo) => !todo.isComplete)
-          .toList()
-          .length);
+class ActiveTodoCount extends StateNotifier<ActiveTodoCountState>
+    with LocatorMixin {
+  ActiveTodoCount() : super(ActiveTodoCountState.intiial());
+
+  // ActiveTodoCountState get state => ActiveTodoCountState(
+  //     activeTodoCount: todoList.state.todos
+  //         .where((Todo todo) => !todo.isComplete)
+  //         .toList()
+  //         .length);
+  @override
+  void update(Locator watch) {
+    final List<Todo> todos = watch<TodoListState>().todos;
+    state = state.copyWith(
+        activeTodoCount:
+            todos.where((Todo todo) => !todo.isComplete).toList().length);
+    super.update(watch);
+  }
 }
